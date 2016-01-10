2025329a93b65f7c879a3b2a15328edf75de8c02
nick <nick@desktop.home.nicknach.net>
2016-01-09 22:18:59 -0600

    adjusted ALLOWED_HOSTS

--- pydemo/settings.py
+++ pydemo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import socket
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tamdwknnwuu+o4sx$m_33oou*=pqm7xx9_s)bm9aynj7wn0_su'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+                 os.environ['OPENSHIFT_APP_DNS'], socket.gethostname(),
+                 '*',
+]
 
 
 # Application definition
