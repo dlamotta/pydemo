64163406eb8d78420daa23a1ac381d5afa4b1efd
nick <nick@desktop.home.nicknach.net>
2016-01-09 20:36:00 -0600

    fixed static files config

--- pydemo/urls.py
+++ pydemo/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    #url(r'(.+\.html)$', 'direct_to_template'),
+    url(r'^$', views.index, name='index'),
+    url(r'^test$', views.test),
+    
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pydemo/views.py
+++ pydemo/views.py
@@ -1,18 +1,17 @@
 import os
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-from . import database
-from pydemo.models import PageView
 
 # Create your views here.
 
 def index(request):
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
+    return render(request, 'index.html')
 
-    return render(request, 'templates/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
-    })
+def test(request):
+    return HttpResponse("Testing...")
+
+def procs(request):
+    return render(request, 'procs.html', {'key': 'value'})
