de87b2c950d21c1818e1a36af28705c2de02497d
nick <nick@desktop.home.nicknach.net>
2016-01-09 19:47:44 -0600

    init

--- pydemo/views.py
+++ pydemo/views.py
@@ -11,7 +11,7 @@ def index(request):
     hostname = os.getenv('HOSTNAME', 'unknown')
     PageView.objects.create(hostname=hostname)
 
-    return render(request, 'welcome/index.html', {
+    return render(request, 'templates/index.html', {
         'hostname': hostname,
         'database': database.info(),
         'count': PageView.objects.count()
